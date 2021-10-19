90b878006c78f0b2a49fe33b5db485ba9be11f80
MartinaLundberg <66014098+MartinaLundberg@users.noreply.github.com>
2021-10-19 16:21:38 +0200

    Update count_pronouns.py

--- count_pronouns.py
+++ count_pronouns.py
@@ -71,11 +71,11 @@ def make_plot(pron):
 def find_pronouns():
     
     stat = {'han': 0,'hon': 0,'den': 0,'det': 0,'denna': 0,'denne': 0,'hen': 0,'total': 0}
-    data = os.listdir("datans")
+    data = os.listdir("dat")
    
     for file in data:
         
-        data_files = 'datans/' + file
+        data_files = 'dat/' + file
         save_text = []
         
         #open file in data
